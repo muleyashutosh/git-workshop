e02458f621563d8ec83667c1cf10f2ef2652ae7e
Ashutosh Muley <muleyashutosh@gmail.com>
2025-01-17 14:11:59 +0530

    Divide Function

--- script.py
+++ script.py
@@ -7,6 +7,10 @@ def multiply(a,b):
 def subtract(a,b):
     return a - b
 
+def divide(a,b):
+    return a // b
+
 print(add(5,10))
 print(multiply(5,10))
 print(subtract(5,10))
+print(divide(5,10))
